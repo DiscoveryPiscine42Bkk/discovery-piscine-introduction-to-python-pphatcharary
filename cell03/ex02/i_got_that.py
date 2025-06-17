4dfc49de52033e3d5773bd6208f4e83372dbc959
pphatcharary <68030186@kmitl.ac.th>
2025-06-17 06:43:28 +0000

    add i_got_that.py

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,11 @@
+def main():
+    first_prompt = "What you gotta say? : "
+    next_prompt = "I got that! Anything glse? : "
+
+    user_input = input(first_prompt)
+
+    while user_input != "STOP" :
+        user_input != input(next_prompt)
+
+        if __name__ == "__name__":
+            main()
