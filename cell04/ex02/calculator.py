7ab08194f371a2e9d43f3ba9dd7a21385830c2cc
pphatcharary <68030186@kmitl.ac.th>
2025-06-17 08:36:28 +0000

    calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,13 @@
+def main():
+    a = int(input("Give me the first number : "))
+    b = int(input("Give me the second number: "))
+
+    print("Thank you !")
+
+    print(f"{a} + {b} = {a + b}")
+    print(f"{a} - {b} = {a - b}")
+    print(f"{a} / {b} = {a / b}")
+    print(f"{a} * {b} = {a * b}")
+
+if __name__ == "__main__":
+    main()
