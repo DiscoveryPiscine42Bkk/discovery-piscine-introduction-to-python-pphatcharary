601644c57e81056457ffd7eaf777fc1e61d7db83
pphatcharary <68030186@kmitl.ac.th>
2025-06-18 04:15:43 +0000

    add play_with_arrays.py

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,12 @@
+def main():
+
+    origianl_array = [2, 8, 9, 48, 8, 22, -12, 2] 
+
+    new_array = [x + 2 for x in origianl_array]
+
+    print("Original array:", origianl_array)
+    print("New array:", new_array)
+
+if __name__ == "__main__":
+    main()
+
